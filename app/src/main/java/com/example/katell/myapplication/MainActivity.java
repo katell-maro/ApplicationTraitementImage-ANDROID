2ccfb266f30bfeb394550f12787c10494b84a321
Unknown <katell.maro@gmail.com>
2017-01-30 13:43:58 +0100

    Prendre photo

--- app/src/main/java/com/example/katell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/katell/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.katell.myapplication;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -12,6 +17,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.graphics.Bitmap.createBitmap;
 import static com.example.katell.myapplication.R.id.imageView;
@@ -126,6 +136,11 @@ public class MainActivity extends AppCompatActivity {
             //permet de lancer un Toast secret
             case R.id.secret:
                 Toast.makeText(this, "It works !", Toast.LENGTH_SHORT).show();
+                break;
+
+            //permet de prendre une photo
+            case R.id.photo:
+                takeImageFromCamera();
                 break;
         }
     }
@@ -339,6 +354,31 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageBitmap(bitmap);
     }
 
+
+
+    /**
+     * Permet d'accéder à l'appareil photo et de prendre une photo
+     * de la mettre ensuite dans le bitmap
+     */
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void takeImageFromCamera() {
+        Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            this.bitmap = imageBitmap;
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
 
 
     /**
